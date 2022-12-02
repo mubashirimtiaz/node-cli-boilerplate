b0418f829f38723cb1d6efb7e13d0320e2632b11
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-12-02 20:14:51 +0500

    added util

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { add, sub } from './utils.js';
+import { add, sub, div, mul } from './utils.js';
 
 console.log(add(3, 7));
 console.log(sub(4, 1));
+console.log(mul(2, 5));
+console.log(div(10, 2));
